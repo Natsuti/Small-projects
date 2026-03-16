ed4a8d6251db2d70e663aa7745435bdf7fe7201e
natsuti <axbex1310@gmail.com>
2026-03-16 14:18:41 -0400

    Mejora en organizacion, y nuevas funciones, sigue incompleto

--- Python/Projects/Calendario.py
+++ Python/Projects/Calendario.py
@@ -19,22 +19,9 @@ MONTHS = (
 )
 
 
-def dibujar(lis, x, y, s):
-    lis[y][x] = s
-
-
 def crear_matriz(lx, ly):
     grid = [[" " for i in range(lx + 1)] for x in range(ly + 1)]
     return grid
-
-
-def crear_grid(lis, lx, ly, cx, cy):
-    for y in range(ly + 1):
-        for x in range(lx + 1):
-            if y % cy == 0:
-                dibujar(lis, x, y, "*")
-            elif x % cx == 0:
-                dibujar(lis, x, y, "*")
 
 
 def mostrar(lis):
@@ -44,13 +31,45 @@ def mostrar(lis):
     print("\n\n")
 
 
-def crear_cosa():
-    while True:
-        for k in grid_c.keys():
-            grid_c[k] = int(input("> "))
-        grid = crear_matriz(grid_c["x"], grid_c["y"])
-        crear_grid(grid, grid_c["x"], grid_c["y"], grid_c["cx"], grid_c["cy"])
-        mostrar(grid)
+def dibujar_pixel(lis, x, y, s):
+    lis[y][x] = s
 
 
-crear_cosa()
+def dibujar_texto(lis, x, y, s, m):
+    if m == "h":
+        for i in range(len(s)):
+            dibujar_pixel(lis, x + i, y, s[i])
+    elif m == "v":
+        for i in range(len(s)):
+            dibujar_pixel(lis, x, y + i, s[i])
+    elif m == "d":
+        for i in range(len(s)):
+            dibujar_pixel(lis, x + i, y + i, s[i])
+
+
+def dibujar_linea_h(lis, ix, iy, w, s):
+    for column in range(w):
+        dibujar_pixel(lis, ix + column, iy, s)
+
+
+def dibujar_linea_v(lis, ix, iy, h, s):
+    for row in range(h):
+        dibujar_pixel(lis, ix, iy + row, s)
+
+
+def dibujar_rectangulo(lis, x, y, w, h, s):
+    dibujar_linea_v(lis, x, y, h, s)
+    dibujar_linea_h(lis, x, y, w, s)
+    dibujar_linea_v(lis, x + w, y, h + 1, s)
+    dibujar_linea_h(lis, x, y + h, w + 1, s)
+
+
+# def dibujar_grid(grid, x,y, w,h, cx,cy, s):
+
+
+while True:
+    for k in grid_c.keys():
+        grid_c[k] = int(input(f"{k}:> "))
+    grid = crear_matriz(grid_c["x"], grid_c["y"])
+    dibujar_rectangulo(grid, 0, 0, grid_c["x"], grid_c["y"], "*")
+    mostrar(grid)
